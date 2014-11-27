a02e2dc77a9796f12465cf204aae4c8a52df2cd2
Zoltán Csontos <zoltan.csontos.dev@gmail.com>
2014-11-27 09:44:52 +0100

    Yet another webhook test

--- front-end/js/mythos.js
+++ front-end/js/mythos.js
@@ -119,6 +119,7 @@ $(document).ready(function() {
             cancClbk = $(this).attr('data-cancel-fn-name'),
             caller = $(this);
         ctx.confirmationModal(caller, confClbk, cancClbk);
+        console.log('test call');
     });
    
 //Loads the correct sidebar on window load,
